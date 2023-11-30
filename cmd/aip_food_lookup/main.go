2c310f3c72001fec740bcd7cabe47774feb5f15a
joe <joe@device42.com>
2023-11-30 06:42:05 -0500

    add food data

--- cmd/aip_food_lookup/main.go
+++ cmd/aip_food_lookup/main.go
@@ -4,8 +4,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var (
+	allFoods map[string]string
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +25,50 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	allFoods = map[string]string{}
+
+	dataFolder := os.Getenv("AIP_DATA_FOLDER")
+	processDirectory(dataFolder)
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func processFile(filePath string) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func processDirectory(directoryPath string) error {
+	err := filepath.Walk(directoryPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && filepath.Ext(path) == ".dat" {
+			fmt.Println("Processing file:", path)
+			if err := processFile(path); err != nil {
+				fmt.Println("Error processing file:", err)
+			}
+		}
+
+		return nil
+	})
+
+	return err
 }
