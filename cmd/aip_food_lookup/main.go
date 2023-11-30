ec05df11adc996ed844c190a5ab9b459914fad8c
joe <joe@device42.com>
2023-11-30 05:23:45 -0500

    more docker and cors

--- cmd/aip_food_lookup/main.go
+++ cmd/aip_food_lookup/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	fmt.Fprint(w, "Hello, this is your Go HTTP service!")
 }
 
