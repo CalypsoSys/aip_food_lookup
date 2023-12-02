ab00affbdaea6fd68c392f7bef2f5af439fcd68a
joe <joe@device42.com>
2023-12-02 09:08:29 -0500

    lookups

--- cmd/aip_food_lookup/go.mod
+++ cmd/aip_food_lookup/go.mod
@@ -1,3 +1,5 @@
 module aip_food_lookup
 
 go 1.18
+
+require github.com/CalypsoSys/godoublemetaphone v0.1.1
--- cmd/aip_food_lookup/main.go
+++ cmd/aip_food_lookup/main.go
@@ -5,17 +5,32 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/CalypsoSys/godoublemetaphone/pkg/godoublemetaphone"
 )
 
+type apiFood struct {
+	allowed                 bool
+	name                    string
+	primaryShortMetaphone   uint16
+	alternateShortMetaphone uint16
+	category                string
+}
+
+type ResponseData struct {
+	PossibleAllowed    []string `json:"possible_allowed"`
+	PossibleDisallowed []string `json:"possible_disallowed"`
+}
+
 var (
-	allFoods       map[string]string
-	allowedFood    map[string]string
-	disAllowedFood map[string]string
+	nameFoosMap map[string]*apiFood
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -24,19 +39,75 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	fmt.Fprint(w, "Hello, this is your Go HTTP service!")
+	key := r.URL.Query().Get("key")
+
+	if key == "" {
+		http.Error(w, "Key parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	response := match(key)
+
+	// Convert the response to JSON
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response headers
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Write the JSON data to the response
+	w.Write(jsonData)
 }
 
 func main() {
-	allFoods = map[string]string{}
-	allowedFood = map[string]string{}
-	disAllowedFood = map[string]string{}
+	nameFoosMap = map[string]*apiFood{}
 
 	dataFolder := os.Getenv("AIP_DATA_FOLDER")
 	processDirectory(dataFolder)
 
+	match("Sel")
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+func match(name string) ResponseData {
+	name = strings.ToLower(name)
+	sdm := godoublemetaphone.NewShortDoubleMetaphone(name)
+	primary := sdm.PrimaryShortKey()
+	possibleAllowed := []string{}
+	possibleDisallowed := []string{}
+	soundPossibleAllowed := []string{}
+	soundPossibleDisallowed := []string{}
+	for k, v := range nameFoosMap {
+		if strings.HasPrefix(k, name) {
+			if v.allowed {
+				possibleAllowed = append(possibleAllowed, v.name)
+			} else {
+				possibleDisallowed = append(possibleDisallowed, v.name)
+			}
+		} else if math.Abs(float64(int(primary)-int(v.primaryShortMetaphone))) < 10 {
+			if v.allowed {
+				soundPossibleAllowed = append(possibleAllowed, v.name)
+			} else {
+				soundPossibleDisallowed = append(possibleDisallowed, v.name)
+			}
+		}
+	}
+
+	if len(possibleAllowed) == 0 && len(soundPossibleAllowed) > 0 {
+		possibleAllowed = soundPossibleAllowed
+	}
+
+	if len(possibleDisallowed) == 0 && len(soundPossibleDisallowed) > 0 {
+		possibleDisallowed = soundPossibleDisallowed
+	}
+
+	return ResponseData{PossibleAllowed: possibleAllowed, PossibleDisallowed: possibleDisallowed}
 }
 
 func getParentFolder(path string) (string, error) {
@@ -62,21 +133,27 @@ func processFile(filePath string) error {
 	category, _ := getFileNameWithoutExtension(filePath)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
-		lower := strings.ToLower(line)
-		if cat, exists := allFoods[lower]; !exists {
-			allFoods[lower] = fmt.Sprintf("%s-%s", allowed, category)
-		} else {
-			if cat != fmt.Sprintf("%s-%s", allowed, category) {
-				fmt.Println(line, cat)
-			}
-			fmt.Println(line)
-		}
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			lower := strings.ToLower(line)
 
-		if allowed == "allowed" {
-			allowedFood[line] = category
-		} else {
-			disAllowedFood[line] = category
+			isAllowed := allowed == "allowed"
+			if cat, exists := nameFoosMap[lower]; !exists {
+				sdm := godoublemetaphone.NewShortDoubleMetaphone(line)
+
+				nameFoosMap[lower] = &apiFood{
+					allowed:                 isAllowed,
+					name:                    line,
+					primaryShortMetaphone:   sdm.PrimaryShortKey(),
+					alternateShortMetaphone: sdm.AlternateShortKey(),
+					category:                category,
+				}
+			} else {
+				if cat.allowed != isAllowed || cat.category != category {
+					fmt.Println(line, cat)
+				}
+				fmt.Println(line)
+			}
 		}
 	}
 
