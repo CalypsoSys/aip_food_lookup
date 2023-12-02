6a50108f0f9be93e49b2b56909c263fbd2a7d7c1
joe <joe@device42.com>
2023-12-02 09:18:39 -0500

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,10 @@ export GOPATH=$GOPATH:$(pwd)
 
 echo "aip_food_lookup"
 #GOOS=linux GOARCH=amd64 go build -o ./docker/aip_food_lookup ./cmd/aip_food_lookup/*.go
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./docker/aip_food_lookup ./cmd/aip_food_lookup/*.go
+cd ./cmd/aip_food_lookup/
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ../../docker/aip_food_lookup .
 
-cd ./docker
+cd ../../docker
 sudo docker build -t aip_food_lookup .
 
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
